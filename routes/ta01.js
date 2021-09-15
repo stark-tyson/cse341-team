0b61a154c0a9db90ede875b798d1bcbcddd259bc
Tyson Stark <starktj22@gmail.com>
2021-09-15 13:50:48 -0700

    test commit

--- routes/ta01.js
+++ routes/ta01.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
   // Request handling
   // CORE CHALLENGE 1 -
   // HTML page is written
-  res.write('<html>');
+  res.write('<html>'); // res stands for response
   res.write('<head><title>Hello Browser!</Title></head>');
   res.write('<body>');
   res.write('<h1>Welcome to my world!</h1>');
