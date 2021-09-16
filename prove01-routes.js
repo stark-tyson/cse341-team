2c95c53350e089e7b2b3925b72f3fa1092eabd74
Tyson Stark <starktj22@gmail.com>
2021-09-16 15:34:48 -0700

    assignment 1 commit

---
+++ prove01-routes.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/create-user') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split('=')[1]);
+            const username = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', username, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+        //res.statusCode = 302;
+        //res.setHeader('Location', '/');
+        //res.end();
+    }
+};
+
+module.exports = requestHandler;
