f7ac157303d1176340622983658dfe653babb175
Tyson Stark <starktj22@gmail.com>
2021-09-22 14:23:22 -0700

    ta02 commit

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,8 +3,26 @@
 const express = require('express');
 const router = express.Router();
 
+
+const userArray = ['Ben', 'Benji', 'Bentholomew']; 
+
+router.post('/ta02/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+  userArray.push(newUser);
+  res.redirect('/ta02/');
+});
+
+router.post('/ta02/removeUser', (req, res, next) => {
+  const remUser = req.body.remUser;
+  const index = userArray.indexOf(remUser);
+  if (index !== -1) {
+    userArray.splice(index, 1);
+  }
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
